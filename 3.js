bb761738c9c90932e26d2c010b626dbf4ba790d9
fniii0 <fanisafirda86@gmail.com>
2024-11-05 22:33:37 +0700

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    document.getElementById("tulisan").innerHTML = `special wish nichhh, hehe!!<br><br>
+    Alhamdulillah, mas bisa bertemu ditanggal ini kembaliii<br><br>
+    bertambahnya usia satu tahun dan jatah hidup juga berkurang satu tahun, selamat berpetualangan di level kehidupan selanjutnyaaa mas, selamat juga berperang dengan pertanyaan 'kapan' yang tidak ada ujungnyaa :)<br><br>
+    
+    semoga panjang umur, sehat selalu,
+    semoga semakin banyak kebahagiaan yang datang setelah ini, semoga diberikan kesabaran lebih buat jalanin kehidupan yang penuh plot twist ini.<br><br>
+    semoga apa yang mas impikan saat ini bisa segera tercapai dan diberikan kemudahan, aamiin.<br><br>
+    U CAN DO IT!! and I will always support u 🫶🏻<br><br>
+    
+    maaf yaa kalau masih banyak kurangnya dari yang aku bikin iniii dikarenakan kondisi badan yg tidak memungkinkann:(, huaaa sangat mendadak bikinnya, mau tau gaa kapan aku bikinnya? eitsss rahasiaaa.<br><br>
+    tahun depan kira-kira aku masih bisa bikin kayak gini ga ya buat mas? Semoga aja 'people come', tapi kita tidak tau kedepannya bakalan kayak gimana. kalau tidak sesuai harapan, ya kita bisa apa, TAPI AKU GAMAUU<br><br>
+    udah ah, takutnya malah nangis hahaha.<br><br> OIYAAA harusnya kamu ngasih foto yang banyakkk, masa cuma ituuu itu aja ss dari profil wa kamuu`;
+
+    const audio = document.getElementById("myAudio");
+    audio.play().catch(function(error) {
+        console.error("Error playing audio:", error);
+    });
+}
